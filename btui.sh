127ffd37cc8f5c91d9c107cce4ac47f709ce8a31
Re'em Kishinevsky <Re'em Kishinevsky>
2025-08-02 13:46:53 +0300

    major improv

--- btui.sh
+++ btui.sh
@@ -1,10 +1,10 @@
 #!/bin/env bash
 ###################################  CONSTS  ###################################
 declare -A COLORS=([black]=0 [maroon]=1 [green]=2 [olive]=3 [navy]=4 [purple]=5 [teal]=6 [silver]=7 [grey]=8 [red]=9 [lime]=10 [yellow]=11 [blue]=12 [fuchsia]=13 [aqua]=14 [white]=15 [grey0]=16 [navyblue]=17 [darkblue]=18 [blue3]=19 [blue3]=20 [blue1]=21 [darkgreen]=22 [deepskyblue4]=23 [deepskyblue4]=24 [deepskyblue4]=25 [dodgerblue3]=26 [dodgerblue2]=27 [green4]=28 [springgreen4]=29 [turquoise4]=30 [deepskyblue3]=31 [deepskyblue3]=32 [dodgerblue1]=33 [green3]=34 [springgreen3]=35 [darkcyan]=36 [lightseagreen]=37 [deepskyblue2]=38 [deepskyblue1]=39 [green3]=40 [springgreen3]=41 [springgreen2]=42 [cyan3]=43 [darkturquoise]=44 [turquoise2]=45 [green1]=46 [springgreen2]=47 [springgreen1]=48 [mediumspringgreen]=49 [cyan2]=50 [cyan1]=51 [darkred]=52 [deeppink4]=53 [purple4]=54 [purple4]=55 [purple3]=56 [blueviolet]=57 [orange4]=58 [grey37]=59 [mediumpurple4]=60 [slateblue3]=61 [slateblue3]=62 [royalblue1]=63 [chartreuse4]=64 [darkseagreen4]=65 [paleturquoise4]=66 [steelblue]=67 [steelblue3]=68 [cornflowerblue]=69 [chartreuse3]=70 [darkseagreen4]=71 [cadetblue]=72 [cadetblue]=73 [skyblue3]=74 [steelblue1]=75 [chartreuse3]=76 [palegreen3]=77 [seagreen3]=78 [aquamarine3]=79 [mediumturquoise]=80 [steelblue1]=81 [chartreuse2]=82 [seagreen2]=83 [seagreen1]=84 [seagreen1]=85 [aquamarine1]=86 [darkslategray2]=87 [darkred]=88 [deeppink4]=89 [darkmagenta]=90 [darkmagenta]=91 [darkviolet]=92 [purple]=93 [orange4]=94 [lightpink4]=95 [plum4]=96 [mediumpurple3]=97 [mediumpurple3]=98 [slateblue1]=99 [yellow4]=100 [wheat4]=101 [grey53]=102 [lightslategrey]=103 [mediumpurple]=104 [lightslateblue]=105 [yellow4]=106 [darkolivegreen3]=107 [darkseagreen]=108 [lightskyblue3]=109 [lightskyblue3]=110 [skyblue2]=111 [chartreuse2]=112 [darkolivegreen3]=113 [palegreen3]=114 [darkseagreen3]=115 [darkslategray3]=116 [skyblue1]=117 [chartreuse1]=118 [lightgreen]=119 [lightgreen]=120 [palegreen1]=121 [aquamarine1]=122 [darkslategray1]=123 [red3]=124 [deeppink4]=125 [mediumvioletred]=126 [magenta3]=127 [darkviolet]=128 [purple]=129 [darkorange3]=130 [indianred]=131 [hotpink3]=132 [mediumorchid3]=133 [mediumorchid]=134 [mediumpurple2]=135 [darkgoldenrod]=136 [lightsalmon3]=137 [rosybrown]=138 [grey63]=139 [mediumpurple2]=140 [mediumpurple1]=141 [gold3]=142 [darkkhaki]=143 [navajowhite3]=144 [grey69]=145 [lightsteelblue3]=146 [lightsteelblue]=147 [yellow3]=148 [darkolivegreen3]=149 [darkseagreen3]=150 [darkseagreen2]=151 [lightcyan3]=152 [lightskyblue1]=153 [greenyellow]=154 [darkolivegreen2]=155 [palegreen1]=156 [darkseagreen2]=157 [darkseagreen1]=158 [paleturquoise1]=159 [red3]=160 [deeppink3]=161 [deeppink3]=162 [magenta3]=163 [magenta3]=164 [magenta2]=165 [darkorange3]=166 [indianred]=167 [hotpink3]=168 [hotpink2]=169 [orchid]=170 [mediumorchid1]=171 [orange3]=172 [lightsalmon3]=173 [lightpink3]=174 [pink3]=175 [plum3]=176 [violet]=177 [gold3]=178 [lightgoldenrod3]=179 [tan]=180 [mistyrose3]=181 [thistle3]=182 [plum2]=183 [yellow3]=184 [khaki3]=185 [lightgoldenrod2]=186 [lightyellow3]=187 [grey84]=188 [lightsteelblue1]=189 [yellow2]=190 [darkolivegreen1]=191 [darkolivegreen1]=192 [darkseagreen1]=193 [honeydew2]=194 [lightcyan1]=195 [red1]=196 [deeppink2]=197 [deeppink1]=198 [deeppink1]=199 [magenta2]=200 [magenta1]=201 [orangered1]=202 [indianred1]=203 [indianred1]=204 [hotpink]=205 [hotpink]=206 [mediumorchid1]=207 [darkorange]=208 [salmon1]=209 [lightcoral]=210 [palevioletred1]=211 [orchid2]=212 [orchid1]=213 [orange1]=214 [sandybrown]=215 [lightsalmon1]=216 [lightpink1]=217 [pink1]=218 [plum1]=219 [gold1]=220 [lightgoldenrod2]=221 [lightgoldenrod2]=222 [navajowhite1]=223 [mistyrose1]=224 [thistle1]=225 [yellow1]=226 [lightgoldenrod1]=227 [khaki1]=228 [wheat1]=229 [cornsilk1]=230 [grey100]=231 [grey3]=232 [grey7]=233 [grey11]=234 [grey15]=235 [grey19]=236 [grey23]=237 [grey27]=238 [grey30]=239 [grey35]=240 [grey39]=241 [grey42]=242 [grey46]=243 [grey50]=244 [grey54]=245 [grey58]=246 [grey62]=247 [grey66]=248 [grey70]=249 [grey74]=250 [grey78]=251 [grey82]=252 [grey85]=253 [grey89]=254 [grey93]=255)
-declare -A KEYS=([UP]=$'\e[A' [DN]=$'\e[B' [CR]="")
+declare -A KEYS=([UP]=$'\e[A' [DOWN]=$'\e[B' [LEFT]=$'\e[C' [RIGHT]=$'\e[D' [CR]="" [ESC]=$'\e')
 
 # Colorscheme
-declare -r COLOR_DEFAULT='#f8f8f8'
+declare -r COLOR_DEFAULT='#f0f0f0'
 declare -r COLOR_SELECTED='#ffaf87'
 
 declare -r DEFAULT_RGB_TRANSITION_DURTATION=0.15
@@ -38,8 +38,9 @@ setabsyx() {
   echo -en "\e[$(($1<0?0:$1));$(($2<0?0:$2))H"
 }
 
-addstr() {
-  setyx 0 0
+_addstr() {
+  local -i dy=${dy-0} dx=${dx-0}
+  setyx "$dy" "$dx"
   echo -en "$*"
 }
 
@@ -58,6 +59,8 @@ getkey() {
 }
 
 ###################################  UTILS  ####################################
+_flush_stdin() { while read -t 0; do read -rsn1; done }
+
 _interpolate_rgb() {
   local r1=$1 g1=$2 b1=$3
   local r2=$4 g2=$5 b2=$6
@@ -111,6 +114,16 @@ _addstr_rgbtrans() {
 }
 
 
+_center() {
+  local str="$1"
+  local -i width=$2
+  local pad_total=$((width - ${#str}))
+  local pad_left=$((pad_total / 2))
+  local pad_right=$((pad_total - pad_left))
+  printf '%*s%s%*s\n' "$pad_left" '' "$str" "$pad_right" ''
+}
+
+
 ################################  MENU ELEMENT  ################################
 
 __erase_menu() {
@@ -118,10 +131,9 @@ __erase_menu() {
   for i in $(seq $#); do
     setyx $((i-1)) 0
     local opt=${!i}
-    # addstr "${#opt}"
-    addstr "  "
-    addstr "$(printf '%*s' ${#opt})"
-    btui::color reset
+    _addstr "  "
+    _addstr "$(printf '%*s' ${#opt})"
+    color reset
   done
   setyx 0 0
 }
@@ -132,15 +144,15 @@ _draw_menu() {
   local -i curr=${curr-0}
   local indicator=${indicator-'>'} text_color=${text_color-$COLOR_DEFAULT} selected_color=${selected_color-$COLOR_SELECTED}
   local labels=("$@")
-  btui::color "$text_color"
+  color "$text_color"
   for ((i = 0; i < ${#labels[@]}; i++)); do
     if [[ $curr == "$i" ]]; then
-      y=$((y+i)) addstr "$(btui::color "$selected_color")$indicator ${labels[i]}$(btui::color "$text_color")"
+      y=$((y+i)) _addstr "$(color "$selected_color")$indicator ${labels[i]}$(color "$text_color")"
     else
-      y=$((y+i)) addstr "  ${labels[i]}"
+      y=$((y+i)) _addstr "  ${labels[i]}"
     fi
   done
-  btui::color reset
+  color reset
   setyx 0 0
 }
 
@@ -158,30 +170,80 @@ _imenu() {
     prev=$curr
     getkey KEY
     case "$KEY" in
-      1) echo one ;;
-      2) echo two ;;
-      "${KEYS[CR]}" | 'l') break ;;
+      "${KEYS[CR]}" | "${KEYS[LEFT]}" | 'l') break ;;
+      "${KEYS[ESC]}" | "${KEYS[RIGHT]}"  | 'h') curr=-1; break ;;
       "${KEYS[UP]}" | 'k') {
-        curr=(${#labels[@]}+curr-1)%${#labels[@]}
+        while :; do
+          curr=(${#labels[@]}+curr-1)%${#labels[@]}
+          [[ -n ${labels[curr]} ]] && break
+        done
        } ;;
-      "${KEYS[DN]}" | 'j') {
-        curr=(curr+1)%${#labels[@]}
+      "${KEYS[DOWN]}" | 'j') {
+        while :; do
+          curr=(curr+1)%${#labels[@]}
+          [[ -n ${labels[curr]} ]] && break
+        done
       } ;;
       *) continue ;;
     esac
-    while read -t 0; do read -rsn1; done
-    y=$((y+curr)) addstr "$(btui::color $COLOR_SELECTED)>"
-    y=$((y+prev)) _addstr_rgbtrans "  ${labels[prev]}" _RGBTRANS_MENU_DESELECT 0.2 &
+    y=$((y+curr)) _addstr "$(color $COLOR_SELECTED)>"
+    y=$((y+prev)) _addstr "  "
+    y=$((y+prev)) x=$((x+2)) _addstr_rgbtrans "${labels[prev]}" _RGBTRANS_MENU_DESELECT "$(bc -l <<< "$DEFAULT_RGB_TRANSITION_DURTATION * 1.5")" &
     y=$((y+curr)) x=$((x+2)) _addstr_rgbtrans "${labels[curr]}" _RGBTRANS_MENU_SELECT
+    _flush_stdin
   done
-  indicator=' ' text_color=\#969696 _draw_menu "${labels[@]}"
+  # shellcheck disable=SC2034
+  (( curr >= 0 )) && outvar="${labels[curr]}" || outvar=""
+  kill $! 2>/dev/null  # Kill last RGB transition subprocess
+}
+
+
+_iswitcher() {
+  local -n outvar="$1"
+  local labels=("${@:2}")
+  local -i x=${x-0} y=${y-0}
+  local -i dx=${dx-0} dy=${dy-0}
+  x+=dx; y+=dy; unset dx dy
+  local -i curr=${curr-0}
+  local -i prev=0
+  local KEY
+  local -i maxlen=0
+  for label in "${labels[@]}"; do
+    if (( maxlen < ${#label} )); then
+      maxlen=${#label}
+    fi
+  done
+  for i in "${!labels[@]}"; do
+    labels[i]=$(_center "${labels[i]}" $maxlen)
+  done
+
+  color \#707070
+  dy=-1 _addstr "$(_center "▲" $maxlen)"
+  dy=1 _addstr "$(_center "▼" $maxlen)"
+  _addstr_rgbtrans "${labels[curr]}" _RGBTRANS_MENU_SELECT
+  color $COLOR_SELECTED
+  while true; do
+    prev=$curr
+    getkey KEY
+    case "$KEY" in
+      "${KEYS[CR]}" | "${KEYS[LEFT]}" | 'l') break ;;
+      "${KEYS[UP]}" | 'k') curr=(${#labels[@]}+curr-1)%${#labels[@]} ;;
+      "${KEYS[DOWN]}" | 'j') curr=(curr+1)%${#labels[@]} ;;
+      *) continue ;;
+    esac
+    _addstr "${labels[curr]}"
+  done
+  _flush_stdin
+  dy=-1 _addstr "$(printf "%${maxlen}s" "")"
+  dy=1 _addstr "$(printf "%${maxlen}s" "")"
   # shellcheck disable=SC2034
   outvar="${labels[curr]}"
+  declare -x CHOICE_IDX=curr
 }
 
 
 ####################################  API  #####################################
-# btui::setpos() {
+# setpos() {
 #   declare -i y=0 x=0
 #   eval set -- "$(getopt -o y:x: -- "$@")"
 #   while true; do
@@ -194,7 +256,7 @@ _imenu() {
 #   setyx 0 0
 # }
 
-btui::color() {
+color() {
   local color="$1"
   if [[ $color == \#* ]]; then
     _readhexcolor "$color" r g b
@@ -206,19 +268,81 @@ btui::color() {
   fi
 }
 
-btui::imenu() {
-  _imenu "$@"
-}
 
-btui::addstr() {
+### Elements ###
+
+menu() {
+  local -i x=${x-0} y=${y-0}
+  local -i dx=${dx-0} dy=${dy-0}
+  x+=dx; y+=dy; unset dx dy
+  local chosen
   eval set -- "$(getopt -o c: -- "$@")"
   while true; do
     case "$1" in
-      -c) c="$2"; shift 2 ;;
+      -c) chosen="$2"; shift 2 ;;
       --) shift; break ;;
     esac
   done
-  addstr "$@"
+  curr=$chosen _draw_menu "$@"
+}
+
+imenu() {
+  local -i x=${x-0} y=${y-0}
+  local -i dx=${dx-0} dy=${dy-0}
+  x+=dx; y+=dy; unset dx dy
+  local chosen
+  eval set -- "$(getopt -o c: -- "$@")"
+  while true; do
+    case "$1" in
+      -c) chosen="$2"; shift 2 ;;
+      --) shift; break ;;
+    esac
+  done
+  curr=$chosen _imenu "$@"
+}
+
+iswitcher() {
+  local -i x=${x-0} y=${y-0}
+  local -i dx=${dx-0} dy=${dy-0}
+  x+=dx; y+=dy; unset dx dy
+  _iswitcher "$@"
+}
+
+addstr() {
+  local -i x=${x-0} y=${y-0}
+  local -i dx=${dx-0} dy=${dy-0}
+  x+=dx; y+=dy; unset dx dy
+  _addstr "$@"
+}
+
+
+fill_rect() {
+  local -i x=${x-0} y=${y-0}
+  local -i dx=${dx-0} dy=${dy-0}
+  x+=dx; y+=dy; unset dx dy
+  local -i w=$1 h=$2
+  local -r fillchar=${3-' '}
+  local -r fill="$(printf "$fillchar%.0s" $(seq $w))"
+  for ((i = 0; i < h; i++)); do
+    y=$i _addstr "$fill"
+  done
+}
+
+draw_box() {
+  local -i x=${x-0} y=${y-0}
+  local -i dx=${dx-0} dy=${dy-0}
+  x+=dx; y+=dy; unset dx dy
+  local -i w=$1 h=$2
+  local -r hchar="─" vchar="│" tl="┌" tr="┐" bl="└" br="┘"
+  local -r hline="$(printf "$hchar%.0s" $(seq $((w-2))))"
+  local -r vlines="$(printf "$vchar%*s$vchar" $((w-2)) )"
+  local -r top="$tl$hline$tr"
+  local -r bot="$bl$hline$br"
+  _addstr "$top"
+  dy=$((w-1)) _addstr "$bot"
+  for ((i = 1; i < h - 1; i++)); do
+    y=$i _addstr "$vlines"
+  done
 }
 
 
